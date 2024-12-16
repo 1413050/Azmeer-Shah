3cefb5077a92e8c9537f86e2db8073f12d8b1a5d
Azmeershah <140653307+Azmeershah@users.noreply.github.com>
2024-12-16 19:10:53 +0500

    asd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,67 @@
+
+
+
+function whereIs($element) {
+  /* Is element visible in viewport? Return "b" if before, "v" if visible, and "a" if after */
+  const rect = $element[0].getBoundingClientRect();
+  if (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= ($(window).height() || $(document).height()) &&
+    rect.right <= ($(window).width() || $(document).width())
+  )
+    return "v"; // Visible
+  if (rect.top < 0 || rect.left < 0) return "b"; // Before
+  return "a"; // After
+}
+
+function loadBars() {
+  let $imgs = $("main img"); // All main images
+  let $bar_before = $("#bar_before ul");
+  let $bar_after = $("#bar_after ul");
+
+  $imgs.each(function () {
+    let $img = $(this);
+    let where_is = whereIs($img);
+
+    /* Append HTML of image to bars */
+    $bar_before.append(
+      `<li ${where_is === "b" ? "" : "class='hidden'"} id="bb_${$img.attr(
+        "id"
+      )}"> <a href="#${$img.attr("id")}"><img src="${$img.attr(
+        "src"
+      )}" alt="${$img.attr("alt")}" /></a></li>`
+    ); // Add class as hidden if is not before
+
+    $bar_after.append(
+      `<li ${where_is === "a" ? "" : "class='hidden'"} id="ba_${$img.attr(
+        "id"
+      )}"> <a href="#${$img.attr("id")}"><img src="${$img.attr(
+        "src"
+      )}" alt="${$img.attr("alt")}" /></a></li>`
+    );
+  });
+}
+
+$(window).on("load", loadBars); // Load bars on page load
+
+window.refreshBars =function refreshBars() {
+  let $imgs = $("main img"); // All main images
+
+  /* For each img */
+  $imgs.each(function () {
+    let $img = $(this);
+    let where_is = whereIs($img);
+
+    /* Change class of images if needed */
+    $(`#bb_${$img.attr("id")}`).attr("class", where_is === "b" ? "" : "hidden"); // Visible if before current - show on *before* bar
+
+    $(`#ba_${$img.attr("id")}`).attr("class", where_is === "a" ? "" : "hidden"); // Visible if after current - show on *after* bar
+  });
+}
+
+$("main#bar_scroll").on("scroll", refreshBars);
+
 $(document).ready(function () {
   const $links = $(".nav-link");
   const $indicator = $(".indicator");
@@ -84,69 +148,7 @@ $(".circle-skillbar").each(function () {
     $number.html(degree + "<span>%</span>");
     $number.css("color", color);
   }, 50);
-});
-
-function whereIs(element) {
-  /* Is element visible in viewport? Return "b" if before, "v" if visible and "a" if after */
-  const rect = element.getBoundingClientRect();
-  if (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  )
-    return "v"; // Visible
-  if (rect.top < 0 || rect.left < 0) return "b"; // Before
-  return "a"; // After
-}
-
-function loadBars() {
-  let imgs = document.querySelectorAll("main img"); // All main images
-  let bar_before = document.querySelector("#bar_before ul");
-  let bar_after = document.querySelector("#bar_after ul");
-  for (let i = 0; i < imgs.length; i++) {
-    let img = imgs[i];
-    let where_is = whereIs(img);
-    /* Append HTML of image to bars */
-    bar_before.insertAdjacentHTML(
-      "beforeend",
-      `<li ${where_is == "b" ? "" : "class='hidden'"} id="bb_${
-        img.id
-      }"> <a href="#${img.id}"><img src="${img.src}" alt="${
-        img.alt
-      }" /></a></li>`
-    ); // Add class as hidden if is not before
-    bar_after.insertAdjacentHTML(
-      "beforeend",
-      `<li ${where_is == "a" ? "" : "class='hidden'"} id="ba_${
-        img.id
-      }"> <a href="#${img.id}"><img src="${img.src}" alt="${
-        img.alt
-      }" /></a></li>`
-    );
-  }
-}
-
-window.onload = loadBars; // Load bars on page load
-
-function refreshBars() {
-  let imgs = document.querySelectorAll("main img"); // All main images
-
-  /* For each img */
-  for (let i = 0; i < imgs.length; i++) {
-    let img = imgs[i];
-    let where_is = whereIs(img);
-
-    /* Change class of images if needed */
-    document.querySelector(`#bb_${img.id}`).className =
-      where_is == "b" ? "" : "hidden"; // Visible if before current - show on *before* bar
-
-    document.querySelector(`#ba_${img.id}`).className =
-      where_is == "a" ? "" : "hidden"; // Visible if before current - show on *after* bar
-  }
-}
-$(document).ready(function() {
+});$(document).ready(function() {
   var aboutActive = $(".aboutActive");
 
   // Function to handle class change
